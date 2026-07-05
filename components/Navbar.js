81f26ad1d87bce48c45ae2b7ca18ac4a17f6f6a4
Sruthy24 <sruthysmnglm@gmail.com>
2026-07-05 20:21:47 +0530

    Add Navbar component for site navigation

---
+++ components/Navbar.js
@@ -0,0 +1,19 @@
+export default function Navbar() {
+
+  return (
+
+<nav className="bg-black text-white">
+
+<div className="max-w-6xl mx-auto flex justify-between items-center p-5">
+
+<h2 className="text-2xl font-bold">
+Next Blog
+</h2>
+
+</div>
+
+</nav>
+
+);
+
+}
