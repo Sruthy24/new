90b76d46317a7e45851a981bbbd9e10c12f855d2
Sruthy24 <sruthysmnglm@gmail.com>
2026-07-06 05:55:08 +0530

    Add Loading component for blog posts

---
+++ app/loading.js
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <div className="text-lg font-medium animate-pulse">
+        Loading blog posts...
+      </div>
+    </main>
+  );
+}
