2772974bfd450a3e71c57d92dca88afee90a375f
Sruthy24 <sruthysmnglm@gmail.com>
2026-07-06 06:01:08 +0530

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="border-t bg-white mt-16">
+      <div className="max-w-7xl mx-auto py-6 px-6 text-center text-gray-500">
+
+        © {new Date().getFullYear()} Next Blog
+
+      </div>
+    </footer>
+  );
+}
